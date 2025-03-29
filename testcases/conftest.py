30ba046f0f2bcdcf292b2933a4e62fade1d3391f
Sajjan <sajjan.maverick@gmail.com>
2025-03-29 15:24:14 +0530

    made some changes

--- testcases/conftest.py
+++ testcases/conftest.py
@@ -10,16 +10,20 @@ from webdriver_manager.firefox import GeckoDriverManager
 @pytest.fixture(params=["chrome", "firefox"], scope="function")
 def driver(request):
     binary_path = ReadConfig.getFirefoxPath()
-
-    if request.param == "chrome":
+    browser = request.param
+    if browser == "chrome":
         options = webdriver.ChromeOptions()
         driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
 
-    elif request.param == "firefox":
+    elif browser == "firefox":
         options = webdriver.FirefoxOptions()
         options.binary_path = binary_path
         driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=options)
 
+    else:
+        raise ValueError("Unsupported browser: " + browser)
+
+    driver.maximize_window()
     yield driver
 
     driver.quit()
